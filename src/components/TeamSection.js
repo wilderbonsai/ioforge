218ca142ea9dc01461f4de50365ea533f55c5e95
Stackbit Code Editor <projects@stackbit.com>
2021-05-07 17:46:08 +0000

    TeamSection.js: changed

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -32,13 +32,13 @@ export default class TeamSection extends React.Component {
                             <header className="card__header">
                               <h3 className="h4 card__title">{person_data.first_name} {person_data.last_name}</h3>
                             </header>
-                            {person_data.link && (
+                            
                             <div className="card__copy">
                               {_.map(actions, (action, action_idx) => (
               <Action key={action_idx} {...this.props} action={action} />
             ))}
                             </div>
-                            )}
+                            }
                             {person_data.bio && (
                             <div className="card__copy">
                               {markdownify(person_data.bio)}
